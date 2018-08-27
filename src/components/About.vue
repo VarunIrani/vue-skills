b09337d2c14265047635077a6ccf7a2996556367
VarunIrani <varunirani0505@gmail.com>
2018-08-27 22:22:28 +0530

    Completed Tutorial

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="about">
+        <h1>Hello {{their_name}}. This is an about page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo commodi, natus sunt quasi facere reprehenderit rerum quae? Eaque earum est eius accusamus enim, doloribus aspernatur odio omnis eveniet a assumenda!</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+  data() {
+    return {
+      their_name: this.$route.params.name
+    };
+  }
+};
+</script>
